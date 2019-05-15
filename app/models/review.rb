c096fd01b6a0e80397eb9962f0495035ec4a1f8c
moral nicolas <mrwallp@gmail.com>
2019-05-15 14:10:57 +0200

    integer&rangeRating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
   validates :content, presence: true
 end
