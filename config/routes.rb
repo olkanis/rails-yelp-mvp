c9dd5fecde82d02ab707f656996ebe43a711ad27
moral nicolas <mrwallp@gmail.com>
2019-05-15 15:31:06 +0200

    ....

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'reviews/content'
   get 'reviews/rating'
     resources :restaurants do
-      resources :reviews, only: [:new, :create]
+      resources :reviews, only: [:index, :new, :create]
     end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
