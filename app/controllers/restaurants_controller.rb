315594ea4c4382fbc799d9cc0c8559c23a70ef42
moral nicolas <mrwallp@gmail.com>
2019-05-15 13:55:14 +0200

    reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -10,6 +10,8 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/1
   # GET /restaurants/1.json
   def show
+          @restaurant = Restaurant.find(params[:id])
+
   end
 
   # GET /restaurants/new
