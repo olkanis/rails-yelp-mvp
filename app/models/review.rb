a95310e8d44d5613158efffb1c9e4b4a6faae801
moral nicolas <mrwallp@gmail.com>
2019-05-15 14:38:41 +0200

    dbseed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 5
+  }
   validates :content, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dish',
+    address:      '1 Bound , Arsenal E2 7JE',
+    description:  'Buzzy destination for Indian street food ',
+    stars:        5
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    stars:        4
+  },
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    stars:        3
+  },
+  {
+    name:         'Sushi',
+    address:      '4th Avenue, NY E55',
+    description:  'Best NY sushi shop!.',
+    stars:        5
+  },
+  {
+    name:         'Mignardises',
+    address:      'Boulevard Gal Doe, Lyon 094',
+    description:  'Gastronomic resto .',
+    stars:        1
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
